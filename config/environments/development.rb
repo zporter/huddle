0c7b66746db974122f6dce6e81163bb593dccd87
Zachary <zporter9@gmail.com>
2010-09-28 17:30:10 -0400

    setup devise for authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Huddle::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # Required for devise and rails
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
