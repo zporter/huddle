e81e930cb8645ca35ff59107711e2233582c0112
Zachary <zporter9@gmail.com>
2010-09-28 17:20:48 -0400

    initial rails setup for huddle app used in rails test prescriptions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huddle::Application.config.secret_token = 'af42d4132dbf99e6d87b15074dd80b3b29558422fdb29d7f82ea1d51e62ee237e844c51c8ee53b8afabef0a7ec7ff75453562551c3eeabaf6d15b093afa0880b'
